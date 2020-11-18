f563ea80cb8ca75190e24bf96cf8d352b7581ddb
Ritam Verma <ritamvvv@gmail.com>
2020-11-18 09:37:22 +0100

    Updates

---
+++ precache-manifest.1b91dc0f92d5775c711e259f1b10f3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c363a28560248d0f60d9173f0a3861d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29f7a1908ea6ae59f113",
+    "url": "./static/css/main.4f291e0b.chunk.css"
+  },
+  {
+    "revision": "30a69a38f3738cacfb56",
+    "url": "./static/js/2.92acc1a2.chunk.js"
+  },
+  {
+    "revision": "78668c460bf9d89b7afc06f7b39eaaa1",
+    "url": "./static/js/2.92acc1a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f7a1908ea6ae59f113",
+    "url": "./static/js/main.1c7c3d4e.chunk.js"
+  },
+  {
+    "revision": "e3227ed0a591f482e9b7",
+    "url": "./static/js/runtime-main.5ddc5c1e.js"
+  }
+]);
